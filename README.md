db99e0b10df4895911de0c375486094ec90e2e8a
Hugo Posca <hugo.posca@gmail.com>
2019-03-02 13:09:26 -0800

    Add POST routes for user
    Currently, only placeholders for user registration and login

--- README.md
+++ README.md
@@ -7,3 +7,8 @@ docker-compose up web
 ```bash
 curl http://localhost:5000/
 ```
+
+```bash
+curl -X POST -d email=hello@world.com&password=asecretpassword http://localhost:5000/registration
+curl -X POST -d email=hello@world.com&password=asecretpassword http://localhost:5000/login
+```
--- main.py
+++ main.py
@@ -2,7 +2,12 @@
 # -*- coding: utf-8 -*-
 
 from flask import Flask
+from flask_restful import Api
 
 app = Flask(__name__)
+api = Api(app)
 
-import views
+import views, resources
+
+api.add_resource(resources.UserRegistration, '/registration')
+api.add_resource(resources.UserLogin, '/login')
